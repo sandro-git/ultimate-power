8257f8fd91aa0f5ea120fd6d2aaa160149ec6eb5
Sandro Das Neves <sandroazerty@icloud.com>
2020-12-29 17:55:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Header></Header>
+  <img
+    src="../src\assets\images\image_principale.webp"
+    alt="image de la salle"
+  />
+  <Maintext></Maintext>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Maintext from "./components/Maintext.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Maintext
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #182d53;
+}
+body {
+  margin: 0;
+}
+
+img {
+  width: 100%;
+}
+</style>
